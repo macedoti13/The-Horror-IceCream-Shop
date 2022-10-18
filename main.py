7d49d1f02f123440e3d529234b163e3775fccecb
Thiago Macedo <thialmacedo@gmail.com>
2022-10-17 21:48:29 -0300

    using the graph

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import graph as g
+
 def main():
-    pass
+    nodes = ["chocolate_branco", "graviola", "kiwi", "papaya_com_cassis", "maca_verde"]
+    graph = g.Graph(nodes, directed=True)
 
 if __name__ == "__main__":
     main()
