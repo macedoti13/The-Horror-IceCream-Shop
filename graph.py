4f71b2e70fb74d0e7235de2be6009ea0c6922d8d
Thiago Macedo <thialmacedo@gmail.com>
2022-10-17 21:33:55 -0300

    init method

--- graph.py
+++ graph.py
@@ -1,2 +1,17 @@
 class Graph:
-    pass
+    
+    def __init__(self, nodes: list, directed: bool=True):
+        """Creates a Graph object.
+
+        Args:
+            nodes (list): list containing the name of the nodes. 
+            directed (bool, optional): Indicates if the graph is of type directed. Defaults to True.
+        """              
+        # number of nodes definition
+        self.nodes = nodes
+        
+        # type of graph definition
+        self.directed = directed
+
+        # adjacency list definition
+        self.adj_list = {node: list() for node in range(self.nodes)}
