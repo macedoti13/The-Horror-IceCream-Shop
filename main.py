de67f5d703693899c88da7fff359957c5b6d7635
Thiago Macedo <thialmacedo@gmail.com>
2022-10-17 21:53:40 -0300

    add_edge

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import graph as g
 def main():
     nodes = ["chocolate_branco", "graviola", "kiwi", "papaya_com_cassis", "maca_verde"]
     graph = g.Graph(nodes, directed=True)
+    graph.add_edge("chocolate_branco", "papaya_com_cassis")
 
 if __name__ == "__main__":
     main()
