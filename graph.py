aea3f14bb15d0a4abcbf52d409d8a8a0dc6bec85
Thiago Macedo <thialmacedo@gmail.com>
2022-10-17 21:39:59 -0300

    add_edge method

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 class Graph:
     
     def __init__(self, nodes: list, directed: bool=True):
-        """Creates a Graph object.
+        """Creates a Graph object with an adjacency list.
 
         Args:
             nodes (list): list containing the name of the nodes. 
@@ -15,3 +15,21 @@ class Graph:
 
         # adjacency list definition
         self.adj_list = {node: list() for node in range(self.nodes)}
+
+    
+    def add_edge(self, node1: str, node2: str):
+        """Connects two nodes with an edge:
+            - If graph is directed: node1 points to node2.
+            - If graph is not directed: node1 points to node2 and node2 points to node1.
+
+
+        Args:
+            node1 (str): Name of the node1.
+            node2 (str): Name of the node2.
+        """        
+        # edge from node1 to node2
+        self.adj_list[node1].append(node2)
+
+        # edge from node2 to node1 (only if not directed)
+        if not self.directed:
+            self.adj_list[node2].append(node1)
