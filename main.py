2fbe7406aa9be20ede950a226bf184c3e9c03fce
Thiago Macedo <thialmacedo@gmail.com>
2022-10-19 19:46:11 -0300

    prettifying the results

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import graph as g
 
 def main():
     graph = g.read_txt('test_files/casocohen10.txt')
-    print(graph.n_two_nodes_edges())
-    print(graph.n_three_nodes_edges())
+    print(f'The number of possible combinations of two flavors is: {graph.n_two_nodes_edges()}')
+    print(f'The number of possible combinations of three flavores is: {graph.n_three_nodes_edges()}')
+    print('\nGraph:')
     graph.print_adj_list()
 
 if __name__ == "__main__":
