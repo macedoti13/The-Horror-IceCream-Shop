ff59a67d489ce646ecb18c4d7b5494eba4c3ac7e
Thiago Macedo <thialmacedo@gmail.com>
2022-11-14 14:47:45 -0300

    new run

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import graph as g
 def main():
     graph = g.read_txt('testfiles/casoleonardo60.txt')
     print(f'The number of possible combinations of two flavors is: {graph.n_two_nodes_edges()}')
-    print(f'The number of possible combinations of three flavores is: {graph.n_three_nodes_edges()}')
+    print(f'The number of possible combinations of three flavors is: {graph.n_three_nodes_edges()}')
     print('\nGraph:')
     graph.print_adj_list()
 
