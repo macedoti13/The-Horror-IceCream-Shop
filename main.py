e064059b16d46561f561d599fb5c7ae07bc2773c
Thiago Macedo <thialmacedo@gmail.com>
2022-11-16 19:26:06 -0300

    removed time

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ import graph as g
 from datetime import datetime
 
 def main():
-    inicio = datetime.now()
     graph = g.read_txt('testfiles/casoleonardo60.txt')
     print(f'The number of possible combinations of two flavors is: {graph.n_two_nodes_edges()}')
     print(f'The number of possible combinations of three flavors is: {graph.n_three_nodes_edges()}')
     print('\nGraph:')
     graph.print_adj_list()
-    final = datetime.now()
-    tempo = final - inicio
-    print()
-    print(f'tempo: {tempo}')
 
 if __name__ == "__main__":
     main()
