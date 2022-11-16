262cfcaf4a5756c26934ce137930996dafdeb1ad
Thiago Macedo <thialmacedo@gmail.com>
2022-11-16 19:25:20 -0300

    new run

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import graph as g
+from datetime import datetime
 
 def main():
-    graph = g.read_txt('testfiles/casoleonardo20.txt')
+    inicio = datetime.now()
+    graph = g.read_txt('testfiles/casoleonardo60.txt')
     print(f'The number of possible combinations of two flavors is: {graph.n_two_nodes_edges()}')
     print(f'The number of possible combinations of three flavors is: {graph.n_three_nodes_edges()}')
     print('\nGraph:')
     graph.print_adj_list()
+    final = datetime.now()
+    tempo = final - inicio
+    print()
+    print(f'tempo: {tempo}')
 
 if __name__ == "__main__":
     main()
